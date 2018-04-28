a8a77d6727bd485e3f615ad46c78f89c01ad8a83
Jim Conner <c_rennochj@outlook.com>
2018-04-28 13:43:09 -0500

    commiting

--- test.py
+++ test.py
@@ -1,32 +1,23 @@
 import lambda_function
 import jimcodb
 
-event1 = {
-    "Details": {
-        "ContactData": {
-            "Attributes": {},
-            "Channel": "VOICE",
-            "ContactId": "4a573372-1f28-4e26-b97b-XXXXXXXXXXX",
-            "CustomerEndpoint": {
-                "Address": "+12108548842",
-                "Type": "TELEPHONE_NUMBER"
-            },
-            "InitialContactId": "4a573372-1f28-4e26-b97b-XXXXXXXXXXX",
-            "InitiationMethod": "INBOUND | OUTBOUND | TRANSFER | CALLBACK",
-            "InstanceARN": "arn:aws:connect:aws-region:1234567890:instance/c8c0e68d-2200-4265-82c0-XXXXXXXXXX",
-            "PreviousContactId": "4a573372-1f28-4e26-b97b-XXXXXXXXXX",
-            "Queue": "QueueName",
-            "SystemEndpoint": {
-                "Address": "+1234567890",
-                "Type": "TELEPHONE_NUMBER"
-            }
-        },
-        "Parameters": {
-            "sentAttributeKey": "sentAttributeValue"
-        }
-    },
-    "Name": "ContactFlowEvent"
-}
+event1 = {'Details':
+              {'ContactData':
+                   {
+                        'Attributes': {},
+                        'Channel': 'VOICE',
+                        'ContactId': 'a33663f2-215b-40de-a969-f38c1c3eb84b',
+                        'CustomerEndpoint': {'Address': '+12108548842', 'Type': 'TELEPHONE_NUMBER'},
+                        'InitialContactId': 'a33663f2-215b-40de-a969-f38c1c3eb84b',
+                        'InitiationMethod': 'INBOUND',
+                        'InstanceARN': 'arn:aws:connect:us-east-1:832433821903:instance/148d3298-163b-4d8e-807b-b9b78b1a31ee',
+                        'PreviousContactId': 'a33663f2-215b-40de-a969-f38c1c3eb84b',
+                        'Queue': None,
+                        'SystemEndpoint': {'Address': '+12066078565', 'Type': 'TELEPHONE_NUMBER'}
+                    },
+               'Parameters': {}
+               }, 
+          'Name': 'ContactFlowEvent'}
 
 context = None
 
