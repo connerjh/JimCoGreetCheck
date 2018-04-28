9f68799268e36036983e3a92dde8557b5d2c6c19
Jim Conner <c_rennochj@outlook.com>
2018-04-28 12:42:32 -0500

    commiting

--- deploy-JimCoGreetCheck.sh
+++ deploy-JimCoGreetCheck.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+aws lambda add-permission --function-name function:JimCoGreetCheck --statement-id 1 \
+--principal connect.amazonaws.com  --action lambda:InvokeFunction --source-account 832433821903 \
+--source-arn arn:aws:connect:us-east-1:832433821903:instance/148d3298-163b-4d8e-807b-b9b78b1a31ee \
+
 git commit -a -m 'commiting'
 
 rm JimCoGreetCheck.zip
