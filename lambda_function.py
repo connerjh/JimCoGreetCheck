51d1b280b9a165a37a345c7366a53f09cf85aa12
Jim Conner <c_rennochj@outlook.com>
2018-04-28 14:13:48 -0500

    commiting

--- lambda_function.py
+++ lambda_function.py
@@ -41,4 +41,4 @@ def lambda_handler(event, context):
 
     preferences = jimcodb.get_greet_check_preferences(event['Details']['ContactData']['CustomerEndpoint']['Address'])
 
-    return {"GreetingType": "XYZGreeting", "Greeting": preferences['Greeting']}
+    return {"GreetingType": "Greeting", "Greeting": preferences['Greeting']}
